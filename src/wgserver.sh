f11a46a3555d14d80a72fed1559e98c13ff1b411
Damir Sh. Yakupov <yds@bk.ru>
2016-07-11 20:43:51 +0400

    fixed persistent keepalive support
    wgserver.sh:
    - added support for 'keepalive' option from server.wg.ini
    - 'keepalive' option checked as number in 10..3600 interval, default
      value is '333' (testing)
    - added 'persistent-keepalive ...' options for wireguard peer setting
    - server send to client own wireguard IP for keepalive starting (fix?)
    - basic configuration ID changed from 'wireguard' to 'saklaw-shelter'
    - fixed identation
    
    wgclient.sh:
    - removed 'keepalive' option support from client.wg.ini - not needed
    - fixed identation

--- src/wgserver.sh
+++ src/wgserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: wgserver.sh 1 2016-07-10 18:56:46+04:00 toor $
+# $Id: wgserver.sh 2 2016-07-11 20:31:13+04:00 toor $
 #
 _bashlyk=saklaw-shelter . bashlyk
 #
@@ -8,101 +8,102 @@ _bashlyk=saklaw-shelter . bashlyk
 #
 udfMain() {
 
-    DEBUGLEVEL=1
-    local dev fn fnLocalKey fnRemoteCrt fnTmp ini ip ipLocal ipRemote keyPeer path pathPri pathPub s
+	DEBUGLEVEL=1
+	local dev fn fnLocalKey fnRemoteCrt fnTmp ini ip ipLocal ipRemote keyPeer path pathPri pathPub timeKeepalive s
 
-    udfThrowOnCommandNotFound cut echo grep ip nc openssl sort wg 
+	udfThrowOnCommandNotFound cut echo grep ip nc openssl sort wg
 
-    path=/etc/saklaw-shelter
-    pathPub=${path}/ssl/public
-    pathPri=${path}/ssl/private
-    ini=${path}/server.wg.ini
+	path=/etc/saklaw-shelter
+	pathPub=${path}/ssl/public
+	pathPri=${path}/ssl/private
+	ini=${path}/server.wg.ini
 
-    udfIni $ini ':dev;port;portAuth;ipLocal ssl:OU'
+	udfIni $ini ':dev;port;portAuth;ipLocal;keepalive ssl:OU'
 
-    : ${dev:=wg0}
-    : ${ipLocal:=10.10.10.1}
-    : ${port:=12912}
-    : ${portAuth:=42912}
+	: ${dev:=wg0}
+	: ${ipLocal:=10.10.10.1}
+	: ${port:=12912}
+	: ${portAuth:=42912}
+	udfIsNumber $keepalive && [[ $keepalive -gt 10 && $keepalive -lt 3600 ]] || keepalive=333
 
-    udfDebug 1 && udfShowVariable dev ipLocal port portAuth OU
-    udfThrowOnEmptyVariable OU
+	udfDebug 1 && udfShowVariable dev ipLocal port portAuth OU
+	udfThrowOnEmptyVariable OU
 
-    if [[ "$1" != "init" ]]; then
+	if [[ "$1" != "init" ]]; then
 
-	ip link del dev $dev 2>/dev/null
-	ip link add dev $dev type wireguard
-	ip address add ${ipLocal}/24 dev $dev
-	wg set $dev private-key <(wg genkey) listen-port $port
-	ip link set up dev $dev
-
-    fi
-
-    fnLocalKey=${pathPri}/${OU}.key
-
-    [[ -n $fnLocalKey && -f $fnLocalKey ]] || eval $(udfOnError exitecho iErrorNoSuchFileOrDir "$fnLocalKey")
-
-    udfMakeTemp fnTmp
-
-    nc -l $portAuth | openssl smime -decrypt -inform PEM -inkey $fnLocalKey | tr -d '\r' > $fnTmp
-
-    [[ $(wc -l < $fnTmp) == 3 ]] || eval $( udfOnError throw iErrorNotValidArgument "client data not valid $(<$fnTmp)" )
-
-    keyPeer="$( tail -n +3 $fnTmp )"
-    s=$( head -n 1 $fnTmp ) 
-
-    rm -f $fnTmp
-
-    for fn in $(ls $pathPub); do
-
-	if [[ -f $pathPub/$fn && $fn =~ .crt ]]; then
-
-	    iSerial="$( grep -Po "Serial Number: \d+ .*" $pathPub/$fn | cut -f 3 -d' ' )"
-
-	else
-
-	    continue
+		ip link del dev $dev 2>/dev/null
+		ip link add dev $dev type wireguard
+		ip address add ${ipLocal}/24 dev $dev
+		wg set $dev private-key <(wg genkey) listen-port $port
+		ip link set up dev $dev
 
 	fi
 
-	[[ $s == $iSerial ]] || continue
+	fnLocalKey=${pathPri}/${OU}.key
 
-	fnRemoteCrt=${pathPub}/$fn
-	break
+	[[ -n $fnLocalKey && -f $fnLocalKey ]] || eval $(udfOnError exitecho iErrorNoSuchFileOrDir "$fnLocalKey")
 
-    done
+	udfMakeTemp fnTmp
 
-    udfDebug 1 && printf "\nremote peer info:\n\tSerial No.\t- %s(%s)\n\twg public key\t- %s\n" "$s" "${fnRemoteCrt##*/}" "$keyPeer"
+	nc -l $portAuth | openssl smime -decrypt -inform PEM -inkey $fnLocalKey | tr -d '\r' > $fnTmp
 
-    if [[ $(wg show $dev | grep peer | wc -l) -ge 253 ]]; then
+	[[ $(wc -l < $fnTmp) == 3 ]] || eval $( udfOnError throw iErrorNotValidArgument "client data not valid $(<$fnTmp)" )
 
-	wg set $dev peer $(wg show $dev latest-handshakes | sort -k 2 -b -n | head -n 1 | cut -f 1) remove
+	keyPeer="$( tail -n +3 $fnTmp )"
+	s=$( head -n 1 $fnTmp )
 
-    fi
+	rm -f $fnTmp
 
-    ipRemote=$( s="$( wg show $dev allowed-ips )"; for ((i=2; i<=254; i++)); do ip="${ipLocal%.*}.$i"; [[ $s != *${ip}/32* ]] && echo $ip && break; done )
+	for fn in $(ls $pathPub); do
 
-    if wg set $dev peer "$keyPeer" allowed-ips "${ipRemote}/32"; then
+		if [[ -f $pathPub/$fn && $fn =~ .crt ]]; then
 
-	echo "OK:$( wg show $dev private-key | wg pubkey ):$( wg show $dev listen-port ):${ipRemote}"
+			iSerial="$( grep -Po "Serial Number: \d+ .*" $pathPub/$fn | cut -f 3 -d' ' )"
 
-    else
+		else
 
-	echo ERROR
+			continue
 
-    fi | openssl smime -encrypt -outform PEM $fnRemoteCrt | nc -l $portAuth
+		fi
 
-    echo "server configuration completed"
+		[[ $s == $iSerial ]] || continue
 
-    printf "\n\nWireguard interface %s info:\n-----------------------------\n\n" "$dev"
-    wg
-    printf "\n\nWireguard interface %s configuration:\n--------------------------------------\n\n" "$dev"
+		fnRemoteCrt=${pathPub}/$fn
+		break
 
-    wg showconf $dev | tee $path/${OU}.${dev}.conf
+	done
 
-    printf "\n----\n"
+	udfDebug 1 && printf "\nremote peer info:\n\tSerial No.\t- %s(%s)\n\twg public key\t- %s\n" "$s" "${fnRemoteCrt##*/}" "$keyPeer"
 
-    chmod 0600 ${path}/${OU}.${dev}.conf
+	if [[ $(wg show $dev | grep peer | wc -l) -ge 253 ]]; then
+
+		wg set $dev peer $(wg show $dev latest-handshakes | sort -k 2 -b -n | head -n 1 | cut -f 1) remove
+
+	fi
+
+	ipRemote=$( s="$( wg show $dev allowed-ips )"; for ((i=2; i<=254; i++)); do ip="${ipLocal%.*}.$i"; [[ $s != *${ip}/32* ]] && echo $ip && break; done )
+
+	if wg set $dev peer "$keyPeer" allowed-ips "${ipRemote}/32" persistent-keepalive $keepalive; then
+
+		echo "OK:$( wg show $dev private-key | wg pubkey ):$( wg show $dev listen-port ):${ipRemote}:${ipLocal}"
+
+	else
+
+		echo ERROR
+
+	fi | openssl smime -encrypt -outform PEM $fnRemoteCrt | nc -l $portAuth
+
+	echo "server configuration completed"
+
+	printf "\n\nWireguard interface %s info:\n-----------------------------\n\n" "$dev"
+	wg
+	printf "\n\nWireguard interface %s configuration:\n--------------------------------------\n\n" "$dev"
+
+	wg showconf $dev | tee $path/${OU}.${dev}.conf
+
+	printf "\n----\n"
+
+	chmod 0600 ${path}/${OU}.${dev}.conf
 
 }
 #

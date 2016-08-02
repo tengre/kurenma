88c291564c3f6aba1f6553cc272eda82dabaf454
Damir Sh. Yakupov <yds@bk.ru>
2016-08-02 15:04:49 +0400

    improved SSL tools curenmake{crt,pem}.sh
    - openssl configurations saved to ../ssl/configs
    - CSR files saved to ../ssl/csr
    - added function '_defLocal' to generate the local variables from comma
      separated list (ini option for udfIni)
    
    curenmakepem.sh:
    - dh1024.pem not used and not created
    
    curenmakecrt.sh:
    - used argument as Common Name
    - used dn.kurenma.ini as source of default values
    
    openssl.cnf.template:
    - updated version from Debian 5 to 7
    - increase default_bits to 2048 ( section [req] )
    
    server.kurenma.ini:
    - replaced section allowedips to ipsAllowed

--- src/curenmakecrt.sh
+++ src/curenmakecrt.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 #
-# $Id: curenmakecrt.sh 19 2016-08-01 17:13:52+04:00 toor $
+# $Id: curenmakecrt.sh 20 2016-08-02 12:55:59+04:00 toor $
 #
 _bashlyk=kurenma . bashlyk
 #
 #
+#
+_defLocal() {
+
+	local s S
+
+	for s in ${*//[;,]/ }; do
+
+		udfIsValidVariable $s || eval $( udfOnError throw iErrorNonValidVariable "$s" )
+		S+="$s "
+
+	done
+
+	echo "local ${S% *}"
+
+}
 #
 udfMain() {
 
@@ -12,21 +27,54 @@ udfMain() {
 
 	[[ -n "$1" ]] || eval $( udfOnError throw iErrorEmptyOrMissingArgument )
 
-	udfThrowOnCommandNotFound chmod openssl
+	udfThrowOnCommandNotFound chmod cp openssl sed
 
-	local conf path pathKey pathCrt
+	local conf fnTemplate path pathCnf pathCrt pathCsr pathKey s csv='C;ST;L;O;OU;CN;EA'
+	eval $(_defLocal $csv)
 
 	path=/etc/kurenma/ssl
-	pathKey=${path}/private
+	pathCsr=${path}/csr
 	pathCrt=${path}/public
-	conf=${path}/kurenma.ssl
+	pathKey=${path}/private
+	pathCnf=${path}/configs
+	conf=${pathCnf}/${1}.kurenma.ssl
+	confDN=${path}/dn.kurenma.ini
+	fnTemplate=${path}/openssl.cnf.template
 #
-	## TODO throw on not exist $conf $path{Crt,Key}
-	## TODO require a filename as CN
-	## TODO use preedit ssl file for use CN as default
+	for s in "$path" "$pathKey" "$pathCrt" "$pathCsr" "$pathCnf"; do
+
+		[[ -d "$s" ]] || eval $( udfOnError throw iErrorNoSuchFileOrDir "$s" )
+
+	done
+
+	for s in "$confDN" "$fnTemplate"; do
+
+		[[ -f $s ]] || eval $( udfOnError throw iErrorNoSuchFileOrDir "$s" )
+
+	done
+
+	cp -f $fnTemplate $conf
+
+	udfIni $confDN ":${csv}"
 #
-	openssl req -new -nodes -keyout ${pathKey}/${1}.key -out ${path}/${1}.csr -config $conf -verbose
-	openssl ca  -in ${path}/${1}.csr -out ${pathCrt}/${1}.crt -config $conf -verbose
+	: ${C:=XX}
+	: ${ST:=State}
+	: ${L:=Locality}
+	: ${O:=Organizational}
+	: ${OU:=OrganizationalUnit}
+	: ${EA:=postmaster@$1}
+
+	OU=${1%%.*}
+	CN=$1
+
+	for s in ${csv//[;,]/ }; do
+
+		sed -i -r -e "s/%${s}%/${!s}/ig" $conf
+
+	done
+#
+	openssl req -new -nodes -keyout ${pathKey}/${1}.key -out ${pathCsr}/${1}.csr -config $conf -verbose
+	openssl ca  -in ${pathCsr}/${1}.csr -out ${pathCrt}/${1}.crt -config $conf -verbose
 #
 	chmod 0600 ${pathKey}/${1}.key
 
--- src/curenmakepem.sh
+++ src/curenmakepem.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 #
-# $Id: curenmakepem.sh 19 2016-08-01 17:29:07+04:00 toor $
+# $Id: curenmakepem.sh 20 2016-08-02 15:04:23+04:00 toor $
 #
 _bashlyk=kurenma . bashlyk
 #
 #
 #
+_defLocal() {
+
+	local s S
+
+	for s in ${*//[;,]/ }; do
+
+		udfIsValidVariable $s || eval $( udfOnError throw iErrorNonValidVariable "$s" )
+		S+="$s "
+
+	done
+
+	echo "local ${S% *}"
+
+}
+#
 udfMain() {
 
-	udfThrowOnCommandNotFound echo chmod mkdir openssl touch wg
+	udfThrowOnCommandNotFound echo chmod mkdir openssl rm sed touch wg
 
-	local conf confRules confTemplate path pathCrt pathKey s C ST L O OU ST EA
+	local conf confDN fnTemplate path pathCnf pathCrt pathKey s csv='C;ST;L;O;OU;CN;EA'
+	eval $(_defLocal $csv)
 #
 	path=/etc/kurenma/ssl
 	pathCrt=${path}/public
 	pathKey=${path}/private
-	conf=${path}/kurenma.ssl
-	confRules=dn.kurenma.ini
-	confTemplate=${path}/openssl.cnf.template
+	pathCnf=${path}/configs
+	conf=${pathCnf}/kurenma.ssl
+	confDN=${path}/dn.kurenma.ini
+	fnTemplate=${path}/openssl.cnf.template
 #
-	mkdir -p ${path}/{public,private,certs,newcerts,crl}
+	mkdir -p ${path}/{public,private,configs,certs,newcerts,crl,csr}
 	chmod 0710 $pathKey
 
-	[[ -f $confTemplate ]] && cp -fv $confTemplate $conf
+	[[ -f $fnTemplate ]] && cp -fv $fnTemplate $conf
 
 	echo $(date "+%s") > ${path}/serial
 
 	rm -f ${path}/index.txt
 	touch ${path}/index.txt
 #
-	udfIni $confRules ':C;ST;L;O;OU;ST;EA'
+	udfIni $confDN ":${csv}"
 #
 	: ${C:=XX}
 	: ${ST:=State}
@@ -37,16 +54,16 @@ udfMain() {
 	: ${O:=Organizational}
 	: ${OU:=OrganizationalUnit}
 	: ${CN:=CommonName}
-	: ${EA:=email@address}
+	: ${EA:=email@domain.name}
 
-	for s in C ST L O OU ST EA; do
+	for s in ${csv//[;,]/ }; do
 
 		sed -i -r -e "s/%${s}%/${!s}/ig" $conf
 
 	done
 #
 	openssl req -nodes -new -x509 -keyout ${pathKey}/cakey.pem -out ${pathCrt}/cacert.pem -days 3650 -config $conf -verbose
-	openssl dhparam -out ${pathCrt}/dh1024.pem 1024
+	#openssl dhparam -out ${pathCrt}/dh2048.pem 2048
 #
 	wg genkey | tee ${pathKey}/private.wg.key | wg pubkey > ${pathCrt}/public.wg.key
 #
